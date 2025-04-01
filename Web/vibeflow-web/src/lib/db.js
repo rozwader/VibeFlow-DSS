0e260e1af45cd605308f433b60c59eb4ac375c7b
rozwader <maciek.rozwadowski2006@gmail.com>
2025-04-01 16:21:34 +0200

    Auth z Hashem i JWT - jeszcze nie dokonczone

---
+++ Web/vibeflow-web/src/lib/db.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2/promise'
+
+let conn;
+export const createConn = async () => {
+    if(!conn){
+        conn = await mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+        })
+    }
+    return conn;
+}
